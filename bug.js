af43952cfec1ee39b6e603a6ea7cd3f01bcaa67b
BugHunter-X <askzenai@gmail.com>
2025-02-19 23:04:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+//Incorrect usage of $inc operator
+db.collection.updateOne({"_id":ObjectId("someId")},{$inc:{counter:1}});
+//Correct usage of $inc operator
+db.collection.updateOne({"_id":ObjectId("someId")},{$inc:{"counter":1}});
+```
